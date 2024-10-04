e94392acb29f4e831e78c4704a9ee3554174648a
Ozonedevops <tosin.adeniyi09@gmail.com>
2024-10-03 21:00:59 -0400

    Equilateral triangle test case

--- TriangleSolver.Tests/Class1.cs
+++ TriangleSolver.Tests/Class1.cs
@@ -12,10 +12,24 @@ namespace TriangleSolver.Tests
     [TestFixture]
     public class TriangleTests
     {
-        
 
-        
+        // Test 1:  Equilateral triangle
+        [Test]
+        public void IdentifyTriangle_EquilateralTriangle_ReturnsTriangle()
+        {
 
-    
+            int firstSide = 4;
+            int secondSide = 4;
+            int thirdSide = 4;
+
+            string expected = "Equilateral triangle";
+            var actual = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+
+            Assert.AreEqual(expected, actual);
+        }
+
+
+
     }
 }
